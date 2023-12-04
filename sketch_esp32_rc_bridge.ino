7f1b143cd3b67eb8f528baf2b21f5715f149ab4c
Xinjue Zou <hit_zou@126.com>
2023-12-04 16:48:15 +0800

    add iic comm

--- sketch_esp32_rc_bridge.ino
+++ sketch_esp32_rc_bridge.ino
@@ -25,41 +25,122 @@
 
 #include "sbus.h"
 #include <HardwareSerial.h>
+#include <Wire.h>
 
+const String VERSION("00.01");
+
+// hardware serial: UART1
 HardwareSerial SBUSS(1);
 
-/* SBUS object, reading SBUS */
+// SBUS object, reading SBUS
 bfs::SbusRx sbus_rx(&SBUSS, 16, 17, true);
-/* SBUS object, writing SBUS */
+// SBUS object, writing SBUS
 bfs::SbusTx sbus_tx(&SBUSS, 16, 17, true);
-/* SBUS data */
+// SBUS data
 bfs::SbusData data;
+
+// RC related
+const uint8_t MAX_CHANNELS = 16;
+int from_min_max_[MAX_CHANNELS][2] = 
+{ 
+  {200, 1800}, 
+  {200, 1800}, 
+  {200, 1800}, 
+  {200, 1800}, 
+  {200, 1800}, 
+  {200, 1800}, 
+  {200, 1800}, 
+  {200, 1800},
+  {994, 1998}, 
+  {994, 1998}, 
+  {994, 1998}, 
+  {994, 1998}, 
+  {994, 1998}, 
+  {994, 1998}, 
+  {994, 1998}, 
+  {994, 1998}
+};
+
+int to_min_max_[MAX_CHANNELS][2] = 
+{ 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100},
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}, 
+  {0, 100}
+};
+
+// iic related
+const int DEVICE_ADDR = 8;
+const uint8_t SEND_DATA_SIZE = MAX_CHANNELS; // 1 byte for ecah rc channel
+const uint8_t RECV_DATA_SIZE = 32; // Wire library has a softlimit up to 32 bytes
+byte send_data_[SEND_DATA_SIZE] = { 0 };
+byte recv_data_[RECV_DATA_SIZE] = { 0 };
+
+// function that executes whenever data is read by master
+// this function is registered as an event, see setup()
+void requestEvent()
+{
+  Wire.write(send_data_, SEND_DATA_SIZE);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int Length)
+{
+  for (int i = 0; i < Length; ++i)
+  {
+    recv_data_[i] = Wire.read();
+  }
+}
 
 void setup() {
   /* Serial to display data */
   Serial.begin(115200);
   while (!Serial) {}
+  Serial.println(VERSION);
+  Serial.println("\nReady");
   /* Begin the SBUS communication */
   sbus_rx.Begin();
   sbus_tx.Begin();
 
-  // inverter test
-  pinMode(22, OUTPUT);
-  digitalWrite(22, HIGH);
+  // iic
+  Wire.begin(DEVICE_ADDR);      // join i2c bus with address
+  Wire.onRequest(requestEvent); // register events
+  Wire.onReceive(receiveEvent);
 }
 
 void loop () {
-  if (sbus_rx.Read()) {
+  if (sbus_rx.Read())
+  {
     /* Grab the received data */
     data = sbus_rx.data();
     /* Display the received data */
-    for (int8_t i = 0; i < data.NUM_CH; i++) {
-      Serial.print(data.ch[i]);
+    for (int8_t i = 0; i < data.NUM_CH; i++)
+    {
+      send_data_[i] = byte(map(int(data.ch[i]), from_min_max_[i][0], from_min_max_[i][1], to_min_max_[i][0], to_min_max_[i][1]));
+#ifdef DEBUG
+      // Serial.print(data.ch[i]);
+      Serial.print(send_data_[i]);
       Serial.print(",");
+#endif
     }
+#ifdef DEBUG
     /* Display lost frames and failsafe data */
     Serial.print(data.lost_frame);
     Serial.print("_");
     Serial.println(data.failsafe);
+#endif
   }
 }
